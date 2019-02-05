15addf1fe5f70b16341bc46d587420e5af28ab2b
Samantha Jeet <samanthajeet@gmail.com>
2019-02-05 09:03:40 -0700

    solved problem

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo'
+import NewTask from './components/NewTask'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      myToDos: ['laundry', 'groceries'],
+      userInput: ''
+    }
+  }
+
+
+
   render() {
+    let list = this.state.myToDos.map( ( task, index ) => {
+      return (
+        <Todo key={ index } task={ task } />
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> My To-Do List: </h1>
+        <NewTask add={this.handleAddTask} />
+        {list}
       </div>
     );
   }
